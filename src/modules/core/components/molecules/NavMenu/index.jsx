27dbb55f9b58eadbba1e418178a0315a71b427b6
Mònica Andrea Rìos Ospina <andre2025rios@gmail.com>
2022-12-09 22:49:55 -0500

    Estilo css a historia 128 y adelantos de historia 47

--- src/modules/core/components/molecules/NavMenu/index.jsx
+++ src/modules/core/components/molecules/NavMenu/index.jsx
@@ -4,72 +4,67 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 
-
-
 export function NavMenu() {
   return (
     <nav className="mvl-navbar">
       <div className="mvl-navbar--left">
-        <NavLink className="mvl-navbar-item mlv-navbar__image" href="#">
-          <img src="/images/marvel_logo.png" className="mvl-navbar__img" alt="compny logo" />
+        <NavLink className="mvl-navbar-item mlv-navbar__image" href="#" to="/">
+          <img src="/images/marvel_logo.png" className="mvl-navbar__img" alt="compny logo"  />
         </NavLink>
 
         <div className="mvl-navbar__actions">
-          <NavLink className="mvl-navbar-item mvl-navbar-item--text" to="/">
+          <NavLink className="mvl-navbar-item mvl-navbar-item--text" to="/Characters" target="_bank" >
             Characters
-          </NavLink>      
+          </NavLink>     
         </div>
       </div>
 
       <div className="Search-container mvl-navbar--right "></div>
-      <NavLink className="mvl-navbar-item--text " to="/search"> 
-        <FontAwesomeIcon icon= {faMagnifyingGlass} className="search-container__icon"/>
-        </NavLink>     
+
+      <div className="Search desktopNav__tabContainer" ></div>
+         <a id="search" className="search desktopNav__tab" aria-label="búsqueda" href="/search"> 
+            <FontAwesomeIcon icon= {faMagnifyingGlass} className="search-container__icon" href="/search" 
+            aria-hidden="true"/> 
+        </a>
     </nav>
   );
 }
 
 function SearchPage() {
   return (
-      <div className="mvl-container-search">    
+      <div className="mvl-container-search">   
       <input className="mvl-container-search-left" type="text" placeholder="SEARCH"/>
-      <FontAwesomeIcon icon= {faMagnifyingGlass} className="search-container__icon2"/>
-      {/* <select className="mvl-container-select-Movie" type="select" id="select">
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-      </select>
-      <select className="mvl-container-select-TV" type="select" id="select">
-      <option value="">none</option>
-        <option value="none"></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-      </select> */}
-     </div>
+      <FontAwesomeIcon icon= {faMagnifyingGlass} className="search-container__icon2"/> 
+       
+        <div className="mvl-container-search-MOVIE">   
+        <select className="mvl-container-select-Movie" multiple size="8"  >
+                <option value="1">lola</option>
+                <option value="2">lola</option>
+                <option value="3">lola</option>
+                <option value="4">lola</option>
+                <option value="5">lola</option>
+                <option value="6">lola</option>
+                <option value="7">lola</option>
+                <option value="8">lola</option>  
 
-   
-        
+                <div className="filters__buttons">
+                <div className='filters__button-left'></div>
+                <div className='filters__button-right'>
+                <button type='button' className="filters__button"></button>
+                <button type='button' className='filters__button'>Cancel</button>
+            </div>
+          </div>
+          </select>
 
-     
-     
-  );
-}
+          <button className='TV' type=""> TV</button>
+
+              <select className="mvl-container-select-TV" type="select"  />
+                 
+            </div>   
+            </div>  
+            
+          
+          );
+        }
 
 export {SearchPage};
-  
-
-    
-
-     
-
-     
- 
