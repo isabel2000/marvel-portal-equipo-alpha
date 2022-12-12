4e7d6616371c36c32aa807d53431ea370dd654f2
Mònica Andrea Rìos Ospina <andre2025rios@gmail.com>
2022-12-11 21:57:57 -0500

    Corrección para unir ramas

--- src/modules/core/components/molecules/Filter/index.jsx
+++ src/modules/core/components/molecules/Filter/index.jsx
@@ -1,118 +1,151 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+// import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+// import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 
-export default function filter() {
-//   const selectBtn = document.querySelector(".select-btn"),
-//   items = document.querySelectorAll(".item");
 
-// selectBtn.addEventListener("click", () => {
-// selectBtn.classList.toggle("open");
-// });
 
-// items.forEach(item => {
-// item.addEventListener("click", () => {
-//     item.classList.toggle("checked");
 
-//     let checked = document.querySelectorAll(".checked"),
-//         btnText = document.querySelector(".btn-text");
 
-//         if(checked && checked.length > 0){
-//             btnText.innerText = `${checked.length} Selected`;
-//         }else{
-//             btnText.innerText = "Select Language";
-//         }
-// });
-// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function filter() {
+// //   const selectBtn = document.querySelector(".select-btn"),
+// //   items = document.querySelectorAll(".item");
+
+// // selectBtn.addEventListener("click", () => {
+// // selectBtn.classList.toggle("open");
+// // });
+
+// // items.forEach(item => {
+// // item.addEventListener("click", () => {
+// //     item.classList.toggle("checked");
+
+// //     let checked = document.querySelectorAll(".checked"),
+// //         btnText = document.querySelector(".btn-text");
+
+// //         if(checked && checked.length > 0){
+// //             btnText.innerText = `${checked.length} Selected`;
+// //         }else{
+// //             btnText.innerText = "Select Language";
+// //         }
+// // });
+// // })
  
-  return (
-  <div className="mvl-character-gri-filters">    
-  <input className="mvl-container-search-left" type="text" placeholder="SEARCH"/> 
-      <FontAwesomeIcon icon= {faMagnifyingGlass} className="search-container__icon"/>
+//   return (
+//   <div className="mvl-character-gri-filters">    
+//   <input className="mvl-container-search-left" type="text" placeholder="SEARCH"/> 
+//       <FontAwesomeIcon icon= {faMagnifyingGlass} className="search-container__icon"/>
 
-      <div className="Container">
-      <div className="Select-btn">
-      <span className="btn-text">MOVIE</span>
-      <span className="arrow-dwn">
-        <i className="fa-solid fa-chevron-down"></i>
-      </span>
-      </div>
+//       <div className="Container">
+//       <div className="Select-btn">
+//       <span className="btn-text">MOVIE</span>
+//       <span className="arrow-dwn">
+//         <i className="fa-solid fa-chevron-down"></i>
+//       </span>
+//       </div>
 
-       <ul className="list-items">
-        <li className="item">
-          <span className="checkbox">
-            <i className="fa-solid fa-check check-icon"></i>
-          </span>
-          <span className="item-text">HTML & CSS </span>
-        </li>
-        <li className="item">
-          <span className="checkbox">
-            <i className="fa-solid fa-check check-icon"></i>
-          </span>
-          <span className="item-text">HTML & CSS </span>
-        </li>
-        <li className="item">
-          <span className="checkbox">
-            <i className="fa-solid fa-check check-icon"></i>
-          </span>
-          <span className="item-text">HTML & CSS </span>
-        </li>
-        <li className="item">
-          <span className="checkbox">
-            <i className="fa-solid fa-check check-icon"></i>
-          </span>
-          <span className="item-text">HTML & CSS </span>
-        </li>
-        <li className="item">
-          <span className="checkbox">
-            <i className="fa-solid fa-check check-icon"></i>
-          </span>
-          <span className="item-text">HTML & CSS </span>
-        </li>
-        <li className="item">
-          <span className="checkbox">
-            <i className="fa-solid fa-check check-icon"></i>
-          </span>
-          <span className="item-text">HTML & CSS </span>
-        </li>
-        <li className="item">
-          <span className="checkbox">
-            <i className="fa-solid fa-check check-icon"></i>
-          </span>
-          <span className="item-text">HTML & CSS </span>
-        </li>
-        <li className="item">
-          <span className="checkbox">
-            <i className="fa-solid fa-check check-icon"></i>
-          </span>
-          <span className="item-text">HTML & CSS </span>
-        </li>
-      </ul> 
-      <div className="filters__buttons">
-        <div className="filters__buttons-lef"> </div>
-        <button className="button-apply" type="button">APPLY</button>
-        <div className="filters__buttons-right"></div>
-        <button className="button-apply" type="button">RESET</button>
-        <button className="button-apply" type="button">CANCEL</button>
+//        <ul className="list-items">
+//         <li className="item">
+//           <span className="checkbox">
+//             <i className="fa-solid fa-check check-icon"></i>
+//           </span>
+//           <span className="item-text">HTML & CSS </span>
+//         </li>
+//         <li className="item">
+//           <span className="checkbox">
+//             <i className="fa-solid fa-check check-icon"></i>
+//           </span>
+//           <span className="item-text">HTML & CSS </span>
+//         </li>
+//         <li className="item">
+//           <span className="checkbox">
+//             <i className="fa-solid fa-check check-icon"></i>
+//           </span>
+//           <span className="item-text">HTML & CSS </span>
+//         </li>
+//         <li className="item">
+//           <span className="checkbox">
+//             <i className="fa-solid fa-check check-icon"></i>
+//           </span>
+//           <span className="item-text">HTML & CSS </span>
+//         </li>
+//         <li className="item">
+//           <span className="checkbox">
+//             <i className="fa-solid fa-check check-icon"></i>
+//           </span>
+//           <span className="item-text">HTML & CSS </span>
+//         </li>
+//         <li className="item">
+//           <span className="checkbox">
+//             <i className="fa-solid fa-check check-icon"></i>
+//           </span>
+//           <span className="item-text">HTML & CSS </span>
+//         </li>
+//         <li className="item">
+//           <span className="checkbox">
+//             <i className="fa-solid fa-check check-icon"></i>
+//           </span>
+//           <span className="item-text">HTML & CSS </span>
+//         </li>
+//         <li className="item">
+//           <span className="checkbox">
+//             <i className="fa-solid fa-check check-icon"></i>
+//           </span>
+//           <span className="item-text">HTML & CSS </span>
+//         </li>
+//       </ul> 
+//       <div className="filters__buttons">
+//         <div className="filters__buttons-lef"> </div>
+//         <button className="button-apply" type="button">APPLY</button>
+//         <div className="filters__buttons-right"></div>
+//         <button className="button-apply" type="button">RESET</button>
+//         <button className="button-apply" type="button">CANCEL</button>
 
  
-      </div>
-      </div>
+//       </div>
+//       </div>
       
-      <div className="Container">
-      <div className="Select-btn"></div>
-      <span className="btn-text">TV</span>
-      <span className="arrow-dwn">
-        <i className="fa-solid fa-chevron-down"></i>
-      </span>
-      </div>
+//       <div className="Container">
+//       <div className="Select-btn"></div>
+//       <span className="btn-text">TV</span>
+//       <span className="arrow-dwn">
+//         <i className="fa-solid fa-chevron-down"></i>
+//       </span>
+//       </div>
       
 
-      </div>
+//       </div>
      
        
-          );
-        }
+//           );
+//         }
 
       
        
